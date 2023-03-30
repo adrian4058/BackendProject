384afc958f91192826069a82c24f36bdb90d76a5
adrian4058 <adrian_2016_@outlook.es>
2023-03-30 00:09:33 -0300

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(6292, () => {
     console.log("%s listening at 6292"); // eslint-disable-line no-console
   });
